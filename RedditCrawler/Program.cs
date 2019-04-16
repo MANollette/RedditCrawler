cc8607e0781f94ee167ef528f9bfd97ab0d25cc8
Michael Nollette <michael.a.nollette@gmail.com>
2019-04-16 06:20:09 -0700

    Incorporated basic method names and console-based user input menu.

--- RedditCrawler/Program.cs
+++ RedditCrawler/Program.cs
@@ -7,6 +7,7 @@ using System.Threading;
 using System.Threading.Tasks;
 using static RedditSharp.Things.VotableThing;
 using System.Security;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace RedditCrawler
 {
@@ -14,12 +15,152 @@ namespace RedditCrawler
     {
         static void Main(string[] args)
         {
+            //Program initialization for method call
+            var p = new Program();
+
+            char c = new Program().MainMenuInput();
+            if (c != 'n')
+            {
+                try
+                {
+                    switch (c)
+                    {
+                        case 'a':
+                            p.NewUser();
+                            break;
+                        case 'b':
+                            p.NewEmail();
+                            break;
+                        case 'c':
+                            p.NewSub();
+                            break;
+                        case 'd':
+                            p.NewSearch();
+                            break;
+                        case 'e':
+                            p.Listen();
+                            break;
+                        case 'f':
+                            Environment.Exit(0);
+                            break;
+                    }
+                }
+                catch(Exception e)
+                {
+                    Console.WriteLine("Error! Details: " + e.Message);
+                    Console.ReadLine();
+                }
+            }
+            else
+            {
+                Console.WriteLine("Returned input from MainMenuInput not valid.");
+            }
+        }
+
+        //Method for initial menu input retrieval
+        private char MainMenuInput()
+        {
+            Console.WriteLine("Welcome to RedditCrawler!\nPlease select from the following menu.\n\n" +
+                              "A. Input new user information\n" +
+                              "B. Input new notification email\n" +
+                              "C. Select a new SubReddit\n" +
+                              "D. Input new search criteria\n" +
+                              "E. Run application with saved settings\n" +
+                              "F. Exit application.");
+            try
+            {
+                string response = Console.ReadLine().ToLower();
+                char cResponse = 'n';
+                if (response.Count() == 1)
+                {
+                    cResponse = response[0];
+                    if (cResponse == 'a' 
+                        || cResponse == 'b'
+                        || cResponse == 'c'
+                        || cResponse == 'd'
+                        || cResponse == 'e'
+                        || cResponse == 'f')
+                    {
+                        return cResponse;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Please input a single character A-F");
+                        MainMenuInput();
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Please input a single character A-F");
+                    MainMenuInput();
+                }              
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine("Error. \n" + e.Message);
+            }
+            return 'n';
+        }
+
+        //Method for initializing or changing the user credentials.
+        private void NewUser()
+        {
+            /*TODO:
+             -Check for existence of user file.
+             -If nonexistent, create.
+             -If blank, amend
+             -If exists, modify credentials
+             -Return to main menu
+             */
+        }
+
+        //Method for changing the subreddit to monitor
+        private void NewSub()
+        {
+            /*TODO:
+             -Check for existence of user file.
+             -If nonexistent, create.
+             -If blank, amend
+             -If exists, modify sub details
+             -Return to main menu
+             */
+        }
+
+        //Method for changing email address
+        private void NewEmail()
+        {
+            /*TODO:
+             -Check for existence of user file.
+             -If nonexistent, create.
+             -If blank, amend
+             -If exists, modify email.
+             -Return to main menu
+             */
+        }
+
+        //Method for initializing, changing, or adding new search criteria
+        private void NewSearch()
+        {
+            /*TODO:
+             -Check for existence of user file.
+             -If nonexistent, create and skip to amending.
+             -If blank, skip to amending.
+             -If exists, list current search criteria
+                -Ask if user would like to add new criteria, or rewrite criteria.
+                -LATER: Add option to remove specific criteria. 
+             -Return to main menu
+             */
+        }
+
+        //Continuous method for monitoring sub.
+        private void Listen()
+        {
             while (true)
             {
                 try
                 {
                     //input needs to be acquired from a locally saved text file after being input, and will be used to filter the results
-                    string input = ""; 
+                    string input = "";
 
                     //List should also be acquired from a locally saved text file, and will consist of
                     //all previous entries the user has already been notified of. 
@@ -34,7 +175,7 @@ namespace RedditCrawler
                     List<string> resultList = new Program().GetPosts(user, password, sub);
 
                     //Remove posts that do not match criteria
-                    foreach(string s in resultList)
+                    foreach (string s in resultList)
                     {
                         if (s == input)
                             resultList.Remove(s);
@@ -61,10 +202,10 @@ namespace RedditCrawler
                     if (resultList.Count > 1)
                     {
                         foreach (string s in resultList)
-                            new Program().notifyUser(s);
+                            new Program().NotifyUser(s);
                     }
                 }
-                catch(Exception ex)
+                catch (Exception ex)
                 {
                     Console.WriteLine(ex.Message);
                     Console.ReadLine();
@@ -72,11 +213,13 @@ namespace RedditCrawler
             }
         }
 
-        void notifyUser(string result)
+        //Method for sending email notification to user.
+        void NotifyUser(string result)
         {
             //code to notify user of the result.
         }
 
+        //Method for retrieving posts from website.
         List<string> GetPosts(string user, string password, string sub)
         {
             Thread.Sleep(30000);
